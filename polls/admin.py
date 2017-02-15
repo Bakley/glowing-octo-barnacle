5f1a05cb8ec0be54572476563a9f3ae4974ee070
Barclay Koin <barclaykoin@gmail.com>
2017-02-15 08:34:10 +0300

    ;-)

--- polls/admin.py
+++ polls/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Question
 
-# Register your models here.
+
+class QuestionAdmin(admin.ModelAdmin):
+    # list_display = ['question_text', 'pud_date']
+    list_filter = ["pub_date"]
+
+admin.site.register(Question, QuestionAdmin)
