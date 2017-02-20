07d8fe470f0c1fa0b6282f75dfbbfdb5e8589e40
Barclay Koin <barclaykoin@gmail.com>
2017-02-20 22:45:13 +0300

    Started a new app to test Django capabilities

--- polls/admin.py
+++ polls/admin.py
@@ -1,9 +1,10 @@
 from django.contrib import admin
-from .models import Question
+from .models import Question, Choice
 
 
 class QuestionAdmin(admin.ModelAdmin):
-    # list_display = ['question_text', 'pud_date']
+    list_display = ('question_text', 'pub_date', 'was_published_recently')
     list_filter = ["pub_date"]
 
 admin.site.register(Question, QuestionAdmin)
+admin.site.register(Choice)
