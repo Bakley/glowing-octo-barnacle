6d01175eca9d37ab5c1bfb4db222a8f2ef769743
Barclay Koin <barclaykoin@gmail.com>
2017-02-24 14:16:17 +0300

    Add new urls for time! changes made for views

--- music/urls.py
+++ music/urls.py
@@ -8,7 +8,7 @@ urlpatterns = [
     url(r'^$', views.index, name='index'),
     url(r'^$', views.details, name='details'),
 
-    url(r'^$', views.time, name='time'),
+    url(r'^$', views.current_time, name='current_time'),
 
     url(r'^$', views.hours_ahead, name='hours_ahead'),
 
--- music/views.py
+++ music/views.py
@@ -1,4 +1,7 @@
+from django.http import HttpResponse
 from django.shortcuts import render
+
+import datetime
 from .models import Album
 
 
@@ -9,3 +12,18 @@ def index(request):
 
 def details(request):
     return render(request, 'music/details.html')
+
+
+def current_time(request):
+    now = datetime.datetime.now()
+    html = "<html><body>It is now {0}.</body></html>".format(now)
+    return HttpResponse(html)
+
+def hours_ahead(request, offset):
+    try:
+        offset = int(offset)
+    except ValueError:
+        raise HTTP404()
+    offset_date = datetime.datetime.now() + datetime.timedelta(hours=offset)
+    html = "<html><body>It is now {0}.</body></html>".format(offset_date)
+    return HttpResponse(html)
