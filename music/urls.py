34f85e867c1f7140ac88ad986d923fcc0f058305
Barclay Koin <barclaykoin@gmail.com>
2017-02-24 14:06:59 +0300

    Add new urls for time!

--- music/urls.py
+++ music/urls.py
@@ -6,5 +6,10 @@ app_name = "music"
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
-    url(r'^$', views.details, name='details')
+    url(r'^$', views.details, name='details'),
+
+    url(r'^$', views.time, name='time'),
+
+    url(r'^$', views.hours_ahead, name='hours_ahead'),
+
 ]
