65cb3f6ee69f51fa4059b5479c4843b30e7c3e0f
Barclay Koin <barclaykoin@gmail.com>
2017-04-05 09:13:18 +0300

    Added Paginator and did some modification on Views

--- blog/views.py
+++ blog/views.py
@@ -1,21 +1,22 @@
 from django.shortcuts import render, get_object_or_404
+# from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from django.views.generic import ListView
 from .models import Post
 
 
-def post_list(request):
-    posts = Post.published.all()
-    context = {'posts': posts}
-    return render(request,
-                  'blog/post/list.html',
-                  context)
+class PostListView(ListView):
+    queryset = Post.published.all()
+    context_object_name = "posts"
+    paginated_by = 3  # 3 posts in each page
+    template_name = 'blog/post/list.html'
 
 
 def post_details(request, year, month, day, post):
     posts = get_object_or_404(Post,
-                             slug=post,
-                             status='published',
-                             publish__year=year,
-                             publish__month=month,
-                             publish__day=day)
+                              slug=post,
+                              status='published',
+                              publish__year=year,
+                              publish__month=month,
+                              publish__day=day)
     context = {'posts': posts}
     return render(request, 'blog/post/details.html', context)
